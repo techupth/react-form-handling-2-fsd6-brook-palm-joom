928de009198e45a96fbfe532df23955dcdf91280
Joom <jnwongsa@gmail.com>
2024-06-05 14:51:00 +0700

    update:ex form-handling2/Joom

--- src/components/ProductForm.jsx
+++ src/components/ProductForm.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react";
+
 function ProductForm() {
+  const [name, setName] = useState ("");
+  const [price,setPrice] = useState ("");
+  const [image, setImage] = useState ("")
+  const [description, setDescription] = useState ("")
+
+  const handleSunmit = () => {
+    const data = {
+      name: name,
+      price: price,
+      image: image,
+      description: description
+    };
+    alert(JSON.stringify(data));
+  }
+
   return (
-    <form className="post-form">
+    <form className="post-form" onSubmit={handleSunmit}>
       <h1>Create Product Form</h1>
       <div className="input-container">
         <label>
@@ -10,7 +27,7 @@ function ProductForm() {
             name="name"
             type="text"
             placeholder="Enter name here"
-            onChange={() => {}}
+            onChange={(event) => {setName(event.target.value)}}
           />
         </label>
       </div>
@@ -22,7 +39,7 @@ function ProductForm() {
             name="image"
             type="text"
             placeholder="Enter image url here"
-            onChange={() => {}}
+            onChange={(event) => {setImage(event.target.value)}}
           />
         </label>
       </div>
@@ -34,7 +51,7 @@ function ProductForm() {
             name="price"
             type="number"
             placeholder="Enter price here"
-            onChange={() => {}}
+            onChange={(event) => {setPrice(event.target.value)}}
           />
         </label>
       </div>
@@ -46,7 +63,7 @@ function ProductForm() {
             name="description"
             type="text"
             placeholder="Enter description here"
-            onChange={() => {}}
+            onChange={(event) => {setDescription(event.target.value)}}
             rows={4}
             cols={30}
           />
